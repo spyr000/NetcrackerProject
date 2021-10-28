6116516a218eda8ce4913654f3b00b4796766af1
Alexandr <mr.zeddd123@gmail.com>
2021-10-28 10:03:27 +0300

    Initial commit

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,10 @@
+package person;
+import java.util.Date;
+import person.Gender;
+
+public class Person {
+    private int id;
+    private	String name;
+    private Date dateOfBirth;
+    private Gender gender;
+}
